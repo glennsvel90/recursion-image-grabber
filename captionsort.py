399e9e14f5f7c9272ea9ae3df348244c092a0ab7
glennsvel90 <glennselwynvel@gmail.com>
2018-04-04 19:31:05 -0400

    complete

--- captionsort.py
+++ captionsort.py
@@ -21,17 +21,17 @@ class CaptionSort:
         self.src_entry.insert(0,'./images')
         ttk.Button(self.mainframe, text = 'Browse...', command = self.browse_src_callback).grid(row = 1, column = 1, sticky = 'w', padx = 5)
 
-        ttk.Label(self.mainframe, text = 'Destination Directory:').grid(row = 3, column = 0, sticky = 'w')
+        ttk.Label(self.mainframe, text = 'Destination Directory:').grid(row = 2, column = 0, sticky = 'w')
         self.dest_entry = ttk.Entry(self.mainframe, width = 54)
-        self.dest_entry.grid(row = 4, column = 0, sticky = 'e')
+        self.dest_entry.grid(row = 3, column = 0, sticky = 'e')
         self.dest_entry.insert(0, './sorted')
-        ttk.Button(self.mainframe, text = 'Browse...', command = self.browse_dest_callback).grid(row = 4, column = 1, sticky = 'w', padx = 5)
+        ttk.Button(self.mainframe, text = 'Browse...', command = self.browse_dest_callback).grid(row = 3, column = 1, sticky = 'w', padx = 5)
 
         self.copy_var = IntVar()
         self.copy_var.set(1)
-        ttk.Checkbutton(self.mainframe, text = 'Copy Files', variable = self.copy_var).grid(row = 5, column = 0, columnspan = 2)
+        ttk.Checkbutton(self.mainframe, text = 'Copy Files', variable = self.copy_var).grid(row = 4, column = 0, columnspan = 2)
 
-        ttk.Button(self.mainframe, text = 'Sort Images', command = self.sort_callback).grid(row = 6, column = 0, columnspan = 2)
+        ttk.Button(self.mainframe, text = 'Sort Images', command = self.sort_callback).grid(row = 5, column = 0, columnspan = 2)
 
 
 
@@ -39,7 +39,6 @@ class CaptionSort:
         path = filedialog.askdirectory(initialdir = self.src_entry.get())
         self.src_entry.delete(0, END)
         self.src_entry.insert(0, path)
-
 
     def browse_dest_callback(self):
         path = filedialog.askdirectory(initialdir = self.dest_entry.get())
@@ -60,7 +59,7 @@ class CaptionSort:
                     #dirpath with the filename
                     image_paths.append(os.path.join(dirpath, file))
 
-        sort_dirs = {}
+        sort_dirs = {} # create dictionary of empty lists for keys A-Z
         for key in range(ord('A'), ord('Z')+1):
             sort_dirs[chr(key)] = []
 
@@ -90,12 +89,12 @@ class CaptionSort:
                     try:
                         os.makedirs(keypath)
                     except os.error as e:
-                        print (str(e))
+                        print(str(e))
                 for file in sort_dirs[key]:
                     if self.copy_var.get():
                         try:
                             shutil.copy(file, os.path.join(keypath, file.split('/')[-1]))
-                        except IOError as e:
+                        except IOError as e: # occurs is destination is not writable
                             print(str(e))
                         else:
                             print('Copied {} to {}'.format(file, keypath))
@@ -103,11 +102,13 @@ class CaptionSort:
                     else:
                         try:
                             shutil.move(file, os.path.join(keypath, file.split('/')[-1]))
-                        except IOError as e:
+                        except IOError as e: # occurs is destination is not writable
                             print(str(e))
                         else:
                             print('Moved {} to {}'.format(file, keypath))
                             sorted_count += 1
+        messagebox.showinfo(title = 'Image Sort Completed',
+                            message = 'Done!\nSorted {} files'.format(sorted_count))
 def main():
     root = Tk()
     gui = CaptionSort(root)
